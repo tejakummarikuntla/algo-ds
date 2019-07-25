1586b24bda77dc9a38e28fb2751f315a610c7db1
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-07-25 17:42:48 +0530

    Add gcd Navie

--- cEra/dev/gcd/gcd_one.py
+++ cEra/dev/gcd/gcd_one.py
@@ -1,21 +1,8 @@
 input_n = input()
 input_lst = [int(x) for x in input_n.split()]
-gcd_lst = []
-gcd_lst_ano = []
-#for dig in input_lst:
-#    for i in range(1,11):
-#        if(dig % i == 0):
-#            gcd_lst.append(i)
-
-min_number = min(input_lst)
-for i in range(1,11):
-    if (min_number % i == 0):
-        gcd_lst.append(i)
-
-for item in gcd_lst:
-    if(max(gcd_lst) % item == 0):
-        gcd_lst_ano.append(item)
-
-print("Min:", gcd_lst)
-print("max:", gcd_lst_ano)
+res = 0
+for i in range(1,input_lst[0] + input_lst[1]):
+    if input_lst[0] % i == 0 and input_lst[1] % i == 0:
+        res = i
+print(res)
 
