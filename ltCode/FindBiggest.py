e859c8f48afc1245e98cc62f8ed0e161f1eccfdb
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-08-06 23:39:53 +0530

    Add Rec

--- ltCode/FindBiggest.py
+++ ltCode/FindBiggest.py
@@ -5,10 +5,21 @@ def FindBiggestNumber(A, n):
             high = digit
     print("Highest Number:", high)
 
+def FindBiggestRec(A, n):
+    index_range = n-1
+    high = A[0]
+
+    if(n == -1):
+        return high
+    else:
+        if A[index_range] > high:
+            high = A[index_range]
+    return FindBiggestRec(A, n-1)
 
 if __name__ == '__main__':
     input_size = int(input())
     print("Enter the array elements:")
     input_lst = [int(x) for x in input().split()]
     assert len(input_lst) == input_size, "Input Size is not equal to array size"
-    FindBiggestNumber(input_lst, input_size)
+    bigg = FindBiggestRec(input_lst, input_size)
+    print("Bigg:", bigg)
