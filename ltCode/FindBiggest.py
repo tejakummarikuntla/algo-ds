216c65d32f3cfc5ebddc91adaed12ab066a27db3
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-08-06 23:53:11 +0530

    Add err

--- ltCode/FindBiggest.py
+++ ltCode/FindBiggest.py
@@ -8,7 +8,7 @@ def FindBiggestNumber(A, n):
 def FindBiggestRec(A, n):
     index_range = n-1
     high = A[0]
-
+### Few issues @FindBiggestRec()
     if(n == -1):
         return high
     else:
@@ -16,10 +16,17 @@ def FindBiggestRec(A, n):
             high = A[index_range]
     return FindBiggestRec(A, n-1)
 
+def maxArrRec(A, n):
+
+    if (n == 1):
+        return A[1]
+    else:
+        return max(A[n-1], maxArrRec(A, n-1))
+
 if __name__ == '__main__':
     input_size = int(input())
     print("Enter the array elements:")
     input_lst = [int(x) for x in input().split()]
     assert len(input_lst) == input_size, "Input Size is not equal to array size"
-    bigg = FindBiggestRec(input_lst, input_size)
+    bigg = maxArrRec(input_lst, input_size)
     print("Bigg:", bigg)
