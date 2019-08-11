88e2e1ea82051e1a24987277aca9fa97f8275234
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-08-11 14:40:17 +0530

    W2 Assign

---
+++ cEra/week2_algorithmic_warmup/1_fibonacci_number/cast/FiboLastDigit.py
@@ -0,0 +1,15 @@
+# Uses python3
+def fibonacci(n):
+    fib_seq = [0, 1]
+
+    for i in range(n):
+        temp_val = ( fib_seq[i] + fib_seq[i + 1] ) % 10
+        fib_seq.append(temp_val)
+
+    return fib_seq[n]
+
+if __name__ == '__main__':
+    input_n = int(input())
+    res = fibonacci(input_n)
+    print(res)
+
