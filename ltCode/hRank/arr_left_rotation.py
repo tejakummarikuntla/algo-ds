1fbb63b321bfd3e24787752592c45b368e115400
Teja Kummarikuntla <34749692+tejakummarikuntla@users.noreply.github.com>
2019-09-01 11:01:42 +0530

    Update arr_left_rotation.py

--- ltCode/hRank/arr_left_rotation.py
+++ ltCode/hRank/arr_left_rotation.py
@@ -1,1 +1,34 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    up_arr  = []
+    up_arr.append(a[d:])
+    up_arr.append(a[:d])
+
+    return up_arr[0]+up_arr[1]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
