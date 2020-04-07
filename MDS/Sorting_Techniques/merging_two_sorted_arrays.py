1386b63ae8c880d5ba815623bdce0eaf610e286d
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2020-04-07 14:21:31 +0530

    Update: Merge sorted arrays

--- MDS/Sorting_Techniques/merging_two_sorted_arrays.py
+++ MDS/Sorting_Techniques/merging_two_sorted_arrays.py
@@ -1,27 +1,26 @@
-
-arr1 = [1, 3, 5, 7, 9]
-arr2 = [2, 4, 6, 8, 10, 11]
-res = []
-def merge(arr1, arr2, len_arr1, len_arr2):
-    i,j = 0,0
+def merging(arr1, arr2):
+    len_arr1 = len(arr1)
+    len_arr2 = len(arr2)
+    k = []
+    i, j = 0, 0
     while(i<len_arr1 and j<len_arr2):
-        if(arr1[i] > arr2[j]):
-            res.append(arr2[j])
-            j += 1
-            # k += 1
-        if(arr1[i] < arr2[j]):
-            res.append(arr1[i])
+        if arr1[i] > arr2[j]:
+            k.append(arr2[j])
+            j +=1 
+        else:
+            k.append(arr1[i])
             i += 1
-            # k += 1
     
-    while(i<len_arr1):
-        res.append(arr1[i])
-        # k += 1
-        i += 1
-    while(j<len_arr2):
-        res.append(arr2[j])
-        # k += 1
-        j += 1
-    print(res)
+    if(i < len_arr1):
+        while(i < len_arr1):
+            k.append(arr1[i])
+            i += 1
+    if(j < len_arr2):
+        while(j < len_arr2):
+            k.append(arr2[j])
+            j += 1
+    
+    return k
 
-merge(arr1, arr2, len(arr1), len(arr2))
+print(merging([2, 6, 7, 9, 10, 13, 15, 17], [1, 3, 5, 8, 11, 14, 16, 19, 22, 25]))
+print(merging([2], [1]))
